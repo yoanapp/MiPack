46a806bd468386e3d821e0ef0735c08d55482558
yoanapp <123227181+yoanapp@users.noreply.github.com>
2024-03-04 14:12:04 -0500

    uddate_login

---
+++ js/login.js
@@ -0,0 +1,55 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Previene el envío del formulario
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+
+           fetch('https://cronometro.onrender.com/api/auth/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: email,
+                password: password
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+          
+             localStorage.setItem('token', data.token);
+     window.location.href = "/views/admin.html";
+          console.log(data)})
+        .catch((error) => console.error('Error:', error));
+
+       /*    fetch('https://cronometro.onrender.com/api/auth/new', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: email,
+                password: password
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+          
+          
+          localStorage.setItem('token', data.token);
+
+          if(data.roll === "profesor"){
+            window.location.href = "/views/index.html";
+          }
+          if(data.roll === "admin"){
+            window.location.href = "/admin";
+          }
+  
+          
+          console.log(data)})
+        .catch((error) => console.error('Error:', error)); */
+    });
+});
